78ac78160b2354c3ff1aeddb47dc55099be588fe
lukakuncak <lukakuncak1@gmail.com>
2024-06-28 20:56:38 +0200

    first commit

---
+++ src/test/java/rs/ac/bg/etf/osrpavicevic/osrpavicevic/OsrpavicevicApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.etf.osrpavicevic.osrpavicevic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrpavicevicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
